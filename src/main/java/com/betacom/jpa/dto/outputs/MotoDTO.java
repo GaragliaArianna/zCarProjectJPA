fddd49fd2514daa75b61e5430b5442ee1fcc7b8b
Betacom <Betacom@BETACOM-PC0JK0.lan>
2026-02-25 15:15:14 +0100

    creato logica crud su moto ma NON implementato con tabelle esterne(freno, colore ecc...)

--- src/main/java/com/betacom/jpa/dto/outputs/MotoDTO.java
+++ src/main/java/com/betacom/jpa/dto/outputs/MotoDTO.java
@@ -1,14 +1,18 @@
 package com.betacom.jpa.dto.outputs;
 
+import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+@Builder
 @Getter
 @Setter
 @ToString
 public class MotoDTO extends VeicoloDTO{
 
+	private Integer id;
+	
     private String targa;
 
     private Integer cc;
