c7ea18b095908a36e9e7b593ac73a35f1b0ac13b
joker2005-code <aldosummo05@gmail.com>
2026-02-25 13:01:26 +0100

    Add files via upload

--- src/main/java/com/betacom/jpa/repositories/TipoSospensioneRepository.java
+++ src/main/java/com/betacom/jpa/repositories/TipoSospensioneRepository.java
@@ -1,12 +1,12 @@
 package com.betacom.jpa.repositories.lookup;
 
-import com.betacom.jpa.models.lookup.Categoria;
+import com.betacom.jpa.models.lookup.TipoSospensione;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-    Optional<Categoria> findByDescrizione(String descrizione);
+public interface TipoSospensioneRepository extends JpaRepository<TipoSospensione, Integer> {
+    Optional<TipoSospensione> findByDescrizione(String descrizione);
     boolean existsByDescrizione(String descrizione);
 }
--- src/main/java/com/betacom/jpa/repositories/TipoVeicoloRepository.java
+++ src/main/java/com/betacom/jpa/repositories/TipoVeicoloRepository.java
@@ -1,12 +1,12 @@
 package com.betacom.jpa.repositories.lookup;
 
-import com.betacom.jpa.models.lookup.Categoria;
+import com.betacom.jpa.models.lookup.TipoVeicolo;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, Integer> {
-    Optional<Categoria> findByDescrizione(String descrizione);
+public interface TipoVeicoloRepository extends JpaRepository<TipoVeicolo, Integer> {
+    Optional<TipoVeicolo> findByDescrizione(String descrizione);
     boolean existsByDescrizione(String descrizione);
 }
