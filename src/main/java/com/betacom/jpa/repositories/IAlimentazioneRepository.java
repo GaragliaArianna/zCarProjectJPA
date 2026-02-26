05061b573533a02dae2f9b5faea600a0ba36b3ba
alfelix <al.felix@libero.it>
2026-02-26 10:25:19 +0100

    annotation @Repository in tutte le classi Repo

--- src/main/java/com/betacom/jpa/repositories/IAlimentazioneRepository.java
+++ src/main/java/com/betacom/jpa/repositories/IAlimentazioneRepository.java
@@ -1,9 +1,11 @@
 package com.betacom.jpa.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.betacom.jpa.models.Alimentazione;
 
+@Repository
 public interface IAlimentazioneRepository extends JpaRepository<Alimentazione, Integer>{
 	
 }
