ccf755d91947c2e66c38d601d74a805c8fae9fcd
GaragliaArianna <arianna.garaglia@gmail.com>
2026-02-26 11:43:26 +0100

    aggiunto controller delete macchina

--- src/main/java/com/betacom/jpa/controllers/MacchinaController.java
+++ src/main/java/com/betacom/jpa/controllers/MacchinaController.java
@@ -5,7 +5,9 @@ import java.util.List;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -87,6 +89,18 @@ public class MacchinaController {
         } catch (Exception e) {
             log.error("Macchina non trovata: {}", id);
             return ResponseEntity.notFound().build();
+        }
+    }
+    
+    // DELETE
+    @DeleteMapping("delete")
+    public ResponseEntity<Void> deleteMacchina(@RequestParam(required=true)  Integer id) {
+        try {
+        	macchinaS.delete(id);
+            return ResponseEntity.noContent().build();
+        } catch (AcademyException e) {
+            log.error("Errore delete macchina {}: {}", id, e.getMessage());
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
         }
     }
 }
