bded8c2f335505129b82e41e6516f47d9b786d09
Francesco Scaltrito <101185088+FrancescoScaltrito@users.noreply.github.com>
2026-02-26 12:58:54 +0100

    Update MotoController.java

--- src/main/java/com/betacom/jpa/controllers/MotoController.java
+++ src/main/java/com/betacom/jpa/controllers/MotoController.java
@@ -17,4 +17,29 @@ public class MotoController {
 
 	private final IMotoServices abbS;
 	private final IMessaggioServices msgS;
+
+	    @GetMapping("/list")
+    public ResponseEntity<Object> list(
+    		@RequestParam(required=false) Integer id,
+    		@RequestParam(required=false)String targa,
+    		@RequestParam(required=false)Integer numeroPorte,
+    		@RequestParam(required=false)Integer cc,
+    		@RequestParam(required=false)String categoria,
+    		@RequestParam(required=false)String colore,
+    		@RequestParam(required=false)String marca,
+    		@RequestParam(required=false)String alimentazione,
+    		@RequestParam(required=false)String tipoVeicolo
+    		){
+    	Object obj = new Object();
+    	HttpStatus status = HttpStatus.OK;
+    	try {
+    		obj = abbS.find(id, targa, cc, categoria, 
+    						colore, marca, alimentazione, tipoVeicolo);
+    	}catch(Exception e) {
+    		obj = e.getMessage();
+    		status = HttpStatus.BAD_REQUEST;
+    	}
+    	return ResponseEntity.status(status).body(obj);
+    	
+    }
 }
