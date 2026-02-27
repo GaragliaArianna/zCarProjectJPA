4a65080c99c5a5c0914bbfb27400918c5d41f313
Francesco Scaltrito <101185088+FrancescoScaltrito@users.noreply.github.com>
2026-02-27 12:54:37 +0100

    Update VeicoloController.java

--- src/main/java/com/betacom/jpa/controllers/VeicoloController.java
+++ src/main/java/com/betacom/jpa/controllers/VeicoloController.java
@@ -1,22 +1,17 @@
 package com.betacom.jpa.controllers;
 
 import java.util.List;
-
-import org.springframework.data.repository.query.Param;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.betacom.jpa.dto.outputs.VeicoloDTO;
 import com.betacom.jpa.dto.outputs.VeicoloFilterDTO;
-import com.betacom.jpa.services.interfaces.IMessaggioServices;
-import com.betacom.jpa.services.interfaces.IMotoServices;
 import com.betacom.jpa.services.interfaces.IVeicoloServices;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -27,9 +22,15 @@ public class VeicoloController {
     private final IVeicoloServices veiS;
 
    
-    @GetMapping("/list")
-    public ResponseEntity<List<VeicoloDTO>> list(@ModelAttribute VeicoloFilterDTO filter) {
-        if (filter == null) filter = new VeicoloFilterDTO(); // opzionale, ma sicuro
+    @PostMapping("/list")
+    public ResponseEntity<List<VeicoloDTO>> list(@RequestBody(required = false) VeicoloFilterDTO filter, 
+                                                  HttpServletRequest request) throws Exception {
+
+        String body = new String(request.getInputStream().readAllBytes());
+        System.out.println("BODY GREZZO: " + body);
+        System.out.println("FILTER RICEVUTO: " + filter);
+        
+        if (filter == null) filter = new VeicoloFilterDTO();
         return ResponseEntity.ok(veiS.find(filter));
     }
 }
